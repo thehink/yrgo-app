3267db59f666ed2527874156a4522ccd6a572665
Thehink <benja280@gmail.com>
2017-05-20 01:58:02 +0200

    Made the news slider responsive

--- src/app/components/NewsFeed/NewsFeed.js
+++ src/app/components/NewsFeed/NewsFeed.js
@@ -24,7 +24,35 @@ export default class NewsFeed extends Component {
       infinite: false,
       speed: 500,
       slidesToShow: 4,
-      slidesToScroll: 4
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        { breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        { breakpoint: 1200,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
 
     return (
